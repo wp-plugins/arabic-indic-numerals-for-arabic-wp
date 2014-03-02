b7dd1ee7b9c0eaf02735effe3379f0d5251c323b
jvarn13 <jvarn13@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-02 12:57:12 +0000

    tagging version 0.16
    git-svn-id: https://plugins.svn.wordpress.org/arabic-indic-numerals-for-arabic-wp/tags/0.16@867981 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ jlv-arabic-indic-numerals.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Arabic-Indic Numerals for Arabic Wordpress
+Plugin URI: http://abuyasmeen.com/arabic-indic-numerals-for-arabic-wordpress/
+Description: Converts numbers in dates into Arabic-Indic numerals
+Version: 0.16
+Author: Jeremy Varnham
+Author URI: http://abuyasmeen.com/
+*/
+
+function convert_numbers_to_arabic( $string ) {
+	$arabic_numbers = array('۰', '۱', '۲', '۳', '٤', '۵', '٦', '۷', '۸', '۹', '.');
+	$english_numbers = array('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.');
+	return str_replace($english_numbers, $arabic_numbers, $string);
+}
+
+add_filter( 'get_the_time', 'make_arabic_time' );
+add_filter( 'get_the_date', 'make_arabic_time' );
+
+function make_arabic_time( $the_time ) {
+	if ( get_bloginfo( 'language' ) == 'ar' ) {
+		$the_time = convert_numbers_to_arabic( $the_time );
+	}
+	return $the_time;
+}
+
+
+?>
